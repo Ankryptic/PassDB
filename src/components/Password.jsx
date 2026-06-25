fb9cd23708157640dc57f4470e9575aec519fd81
Ankit soni <asoni.soni12@gmail.com>
2026-06-25 16:31:20 +0530

    add save button

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SaveBtn from "./SaveBtn";
 
 const Password = () => {
     return (
@@ -10,6 +11,33 @@ const Password = () => {
                     <span>Own</span>
                     <span>Password</span>
                     <span>Manager</span>
+                </div>
+            </div>
+            <div className="section-2 font-roboto w-full flex flex-col items-center gap-2.5">
+                <div className="website-field flex items-center gap-2 w-1/2">
+                    <label className="font-bold" htmlFor="website">Website</label>
+                    <input
+                    className="bg-white w-full" 
+                    type="text" id="website" name="website"/>
+                </div>
+                <div className="user-pass flex gap-2.5 w-1/2">
+                    <div className="user-field ">
+                        <label className="font-bold" htmlFor="username">Username</label>
+                        <input 
+                        className="bg-white"
+                        type="text" id="username" name="username"/>
+                    </div>
+                    <div className="pass-field">
+                        <label className="font-bold" htmlFor="password">Password</label>
+                        <input
+                        className="bg-white"
+                        type="password" id="password" name="password"/>
+                    </div>
+                    <div className="save-btn">
+                        <button>
+                            <SaveBtn/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
