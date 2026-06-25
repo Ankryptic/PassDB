810b8293c9f43bbf7d74f8a79fb44ce0cf5dc1df
Ankit soni <asoni.soni12@gmail.com>
2026-06-25 16:03:11 +0530

    add section1 in password

--- src/components/GithubButton.jsx
+++ src/components/GithubButton.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import gitLogo from '../assets/github.svg'
+import { GithubIcon } from "@animateicons/react/lucide";
 
 
 const GithubButton = () => {
     return (
         <div className="git-btn cursor-pointer border-4 border-black bg-[#c0a9b0] pb-2.5 select-none transition-all duration-100 ease-in-out active:p-0 active:mb-2.5 active:translate-y-2.5">
             <button className="cursor-pointer bg-[#7880b5] flex items-enter gap-3 border-4 border-white  p-1">
-                <img className="invert w-6" src={gitLogo} alt="GitHub-Image" />
+                <GithubIcon
+                    size={24}
+                    duration={1}
+                    color="#ffffff"
+                />
                 <span>GitHub</span>
             </button>
         </div>
--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const Password = () => {
     return (
         <div className="h-[82vh]">
-            
+            <div className="section-1 font-blackOps w-full h-[20%] text-[#bcc4db] text-shadow-[2px_1px_5px_black] flex items-center justify-center">
+                <span className="text-6xl">PassDB</span>
+                <div className="flex flex-col items-start text-sm leading-2.5">
+                    <span>Your</span>
+                    <span>Own</span>
+                    <span>Password</span>
+                    <span>Manager</span>
+                </div>
+            </div>
         </div>
     )
 }
