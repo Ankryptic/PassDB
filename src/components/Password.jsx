979cef349b685449b3eb6cd69483d98f077cfbbe
Ankit soni <asoni.soni12@gmail.com>
2026-06-27 15:47:00 +0530

    add table for passwords

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -18,15 +18,15 @@ const Password = () => {
     return (
         <div className="h-[82vh]">
             <div className="section-1 font-blackOps w-full h-[20%] text-[#bcc4db] text-shadow-[2px_1px_5px_black] flex items-center justify-center">
-                <span className="text-6xl">PassDB</span>
-                <div className="flex flex-col items-start text-sm leading-2.5">
+                <span className="text-6xl select-none">PassDB</span>
+                <div className="flex flex-col items-start text-sm leading-2.5 select-none">
                     <span>Your</span>
                     <span>Own</span>
                     <span>Password</span>
                     <span>Manager</span>
                 </div>
             </div>
-            <div className="section-2 font-blackOps w-full flex flex-col items-center gap-2.5">
+            <div className="section-2 font-blackOps w-full flex flex-col items-center gap-2.5 mb-10">
                 <div className="website-field flex items-center gap-2 w-1/2">
                     <label className="text-shadow-[2px_1px_5px_black] text-[#bcc4db]" htmlFor="website">Website</label>
                     <input
@@ -69,6 +69,42 @@ const Password = () => {
                             <SaveBtn />
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="section-3 w-full flex flex-col items-center justify-center">
+
+                    <div className="w-1/2 bg-[#7880b5] my-2.5 border border-[#c0a9b0] text-center text-[#bcc4db] text-shadow-[2px_1px_5px_black]">
+                        <div className="font-blackOps">Your Passwords</div>
+                    </div>
+                <div className="w-1/2 bg-[#7880b5] border border-[#c0a9b0]">
+                    <table class="table-fixed">
+                        <thead>
+                            <tr>
+                                <th>Website</th>
+                                <th>Username</th>
+                                <th>Password</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                                <td>Malcolm Lockyer</td>
+                                <td>1961</td>
+                            </tr>
+                            <tr>
+                                <td>Witchy Woman</td>
+                                <td>The Eagles</td>
+                                <td>1972</td>
+                            </tr>
+                            <tr>
+                                <td>Shining Star</td>
+                                <td>Earth, Wind, and Fire</td>
+                                <td>1975</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
