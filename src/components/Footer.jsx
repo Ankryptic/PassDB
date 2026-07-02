1292ca464ce0a880cd5d9586468c6ba296deb900
Ankit soni <asoni.soni12@gmail.com>
2026-07-02 14:12:29 +0530

    add footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="flex flex-col items-center justify-center">
-            <div>Created By AKKI</div>
-            <div>&copy; 2026</div>
+        <div className="flex flex-col items-center justify-center font-blackOps text-[#bcc4db] text-shadow-[2px_1px_5px_black]">
+            <div>Built and designed by Akki. </div>
+            <div>All rights reserved. ©</div>
         </div>
     )
 }
